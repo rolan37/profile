ac9cd93f16e2c525c50703198597c0a6df5bed53
rolan37 <rolanpereira3@gmail.com>
2023-06-17 13:38:54 +0530

    Updated profile
    Profile updated with react components

---
+++ src/routes/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function Profile(){
+    return(
+        <div className="main">        
+            <li className="profile_name">Electronics Engineer | STEM Mentor</li>
+            <li></li>
+            <li>Creator of Electronics Simplified</li>
+            <li>An Enthusiastic Learner</li>
+            <li>You can find my Work on github @rolan37</li>
+    </div>
+    )
+}
